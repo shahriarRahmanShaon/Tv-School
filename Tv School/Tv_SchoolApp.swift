df823a7450800da54e1fbf5c8237c35ff303911c
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-22 01:29:43 +0600

    Initial Commit

---
+++ Tv School/Tv_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tv_SchoolApp.swift
+//  Tv School
+//
+//  Created by sergio shaon on 22/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Tv_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
